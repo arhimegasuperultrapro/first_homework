9060e20da4377fa152a7d6785dd3a117446435b9
arhimegasuperultrapro <125913569+arhimegasuperultrapro@users.noreply.github.com>
2024-10-09 09:37:56 +0100

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,4 @@
+completed_homeworks = 12
+hours = 1.5
+name_of_course = 'Python'
+print(f'Курс: {name_of_course}, всего задач: {completed_homeworks}, затрачено часов: {hours}, среднее время выполнения: {hours/completed_homeworks}')
