a0067f48b99e7cca4c61c53499ed8cc150c35520
arhimegasuperultrapro <125913569+arhimegasuperultrapro@users.noreply.github.com>
2024-10-14 14:11:51 +0100

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+# 2
+my_dict = {'Sigma': '48fs910hfi', 'Alfa': 'Hifi4n892', 'Gigachad': '838hhhfu'}
+print(my_dict)
+print(my_dict.get('Sigma'), my_dict.get('omega'))
+my_dict['omega'] = 'superdestroyer228'
+my_dict['Beta'] = 'rmmmmmr4'
+print(my_dict.pop('Gigachad'))
+print(my_dict)
+
+# 3
+my_set = {True, True, False, False, 0, 1}
+print(my_set)
+my_set.update({333, 'arhimaega'})
+print(my_set)
+my_set.remove(1)
+print(my_set)
