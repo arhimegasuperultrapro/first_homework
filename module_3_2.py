6ade10c3fa12872a59930c69351afacf68314daa
arhimegasuperultrapro <125913569+arhimegasuperultrapro@users.noreply.github.com>
2024-10-27 13:12:39 +0000

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,38 @@
+from re import match
+from turtledemo.paint import switchupdown
+
+
+def send_email(message, recipient, *, sender = "university.help@gmail.com"):
+
+    if ('@' not in recipient and '@' not in sender):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+        return 0
+
+    elif (recipient[-4:] == '.com' or recipient[-3:] == '.ru' or recipient[-4:] == '.net')\
+    and (sender[-4:] == '.com' or sender[-3:] == '.ru' or sender[-4:] == '.net'):
+        pass
+    else:
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+        return 0
+
+
+
+    if recipient == sender:
+        print(f"Нельзя отправить письмо самому себе!")
+        return 0
+    if sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+        return 0
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+        return 0
+
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
