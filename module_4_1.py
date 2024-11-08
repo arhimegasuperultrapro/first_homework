5dcad15da9c93462ad9007c0afca38c11a8544e4
arhimegasuperultrapro <125913569+arhimegasuperultrapro@users.noreply.github.com>
2024-11-08 10:50:10 +0000

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,6 @@
+import true_math as tm
+import fake_math as fm
+
+print(tm.divide(1, 0))
+print(fm.divide(1, 0))
+
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+def divide(first, second):
+    try:
+        return first / second
+    except ZeroDivisionError:
+        return inf
