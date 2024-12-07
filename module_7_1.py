6033e83d85a7c8191ff9250cb1275d63dfd655c8
arhimegasuperultrapro <125913569+arhimegasuperultrapro@users.noreply.github.com>
2024-12-07 14:22:55 +0000

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,43 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = "products.txt"
+    def get_products(self):
+        file = open(self.__file_name, "r")
+        info = file.read()
+        file.close()
+        return info
+    def add(self, *products:Product):
+        file = open(self.__file_name, 'a')
+        for i in products:
+            file.write(f"{i.name}, {i.weight}, {i.category}\n") if i.name not in self.get_products() else print(f"Продукт {i.name} уже есть в магазине")
+        file.close()
+
+
+s1 = Shop()
+
+p1 = Product('Potato', 50.5, 'Vegetables')
+
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+
+
+print(p2) # __str__
+
+
+
+s1.add(p1, p2, p3)
+
+
+
+print(s1.get_products())
