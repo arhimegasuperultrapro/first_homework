8615936bcab827c6decde405ae3413f7fb72eb0f
arhimegasuperultrapro <125913569+arhimegasuperultrapro@users.noreply.github.com>
2025-01-08 16:33:12 +0000

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,35 @@
+from random import choice
+
+#1
+first = 'Мама мыла раму'
+
+second = 'Рамена мало было'
+print(list(map(lambda x, y: x==y, first, second)))
+
+#2
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        file = open(file_name, "a", encoding='utf-8')
+        for i in data_set:
+            file.write(str(i))
+        file.close()
+    return write_everything
+
+write = get_advanced_writer('example.txt')
+
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+#3
+class MysticBall:
+    def __init__(self, *words):
+        self.words_collection = words
+    def __call__(self):
+        return choice(self.words_collection)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+
+print(first_ball())
+
+print(first_ball())
+
+print(first_ball())
